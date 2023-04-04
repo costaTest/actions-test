46e1207b8b74e26fc5530741d8016ab55703369c
costaTest <101972094+costaTest@users.noreply.github.com>
2023-04-04 14:19:02 +0000

    Fixed linter issues

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # actions-test
+
+[![GitHub Super-Linter](https://github.com/costatest/actions-test/workflows/actions-test/badge.svg)](https://github.com/marketplace/actions/super-linter)
+
+Testing GitHub Actions
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #!/user/bin/python3
 
 def main():
-    names = ['Tom', 'Dick', 'Harry']
+    names = ["Tom", "Dick", "Harry"]
     for n in names:
         print("Hey", n)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
